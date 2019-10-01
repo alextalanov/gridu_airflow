096a95a65409cc93fa1f7169924cadc2584d932b
Alexander Talanov <alexandr.talanov@kohls.com>
2019-10-01 20:07:14 +0300

    add branching and trigger rule

--- dags/jobs_dag.py
+++ dags/jobs_dag.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.bash_operator import BashOperator
 
 
 def check_table_exist():
@@ -13,13 +14,15 @@ def create_dag(dag_id, start_date, table, database, schedule_interval=None):
     with DAG(dag_id=dag_id, start_date=datetime.fromisoformat(start_date), schedule_interval=schedule_interval) as dag:
         start = PythonOperator(task_id='start',
                                python_callable=lambda: print(f"{dag_id} start processing tables in DB: {database}"))
+
+        who_am_i = BashOperator(task_id='who_am_i', bash_command='whoami')
         check_exist = BranchPythonOperator(task_id="check_table_exist", python_callable=check_table_exist)
         create_table = DummyOperator(task_id='create_table')
         do_nothing = DummyOperator(task_id='do_nothing')
-        insert_new_row = DummyOperator(task_id='insert_new_row', trigger_rule='none_failed')
+        insert_new_row = DummyOperator(task_id='insert_new_row', trigger_rule='all_done')
         query_the_table = DummyOperator(task_id='query_the_table')
 
-    start >> check_exist >> [create_table, do_nothing] >> insert_new_row >> query_the_table
+    start >> who_am_i >> check_exist >> [create_table, do_nothing] >> insert_new_row >> query_the_table
 
     return dag
 
